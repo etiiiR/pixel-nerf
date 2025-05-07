bc5f2c4ed4257ad9e0ef770e6071bb207f98f8f3
Etienne Roulet <etienne.roulet@outlook.com>
2025-05-07 22:15:36 +0200

    Update train.py

--- train/train.py
+++ train/train.py
@@ -12,7 +12,6 @@ sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
 )
 from dotenv import load_dotenv
-
 import warnings
 import trainlib
 from model import make_model, loss
